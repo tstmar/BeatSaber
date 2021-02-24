723f7d0619262061b0a55966ac7968bf3b3ad1d4
Blanka Pop <csikiblanka@gmail.com>
2021-02-24 09:18:49 +0200

    Initial commit

---
+++ Assets/MoveForward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var old = transform.position;
+        //transform.position -= Time.deltaTime * transform.position * 2;
+        transform.position -= new Vector3(0, 0, Time.deltaTime * transform.position.z * 2);
+        Debug.Log($"old: {old}, new: {transform.position}, deltaTime: {Time.deltaTime}");
+        if (transform.position.z <= 0.1f)
+        {
+            Debug.Log($"Cube position is zero, cube is destroyed");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Saber.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+    public LayerMask layer;
+    private Vector3 previousPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1, layer))
+        {
+            //if (Vector3.Angle(transform.position - previousPosition, hit.transform.up) > 90)
+            {
+                Debug.Log($"Saber hit object {hit}");
+                GameObject cube = hit.transform.gameObject;
+                var rigidBody = cube.GetComponent<Rigidbody>();
+                if(rigidBody)
+                {
+                    rigidBody.AddExplosionForce(10.0f, cube.transform.position, 5.0f, 1.0f, ForceMode.Impulse);
+                }
+
+                Destroy(cube, 1.0f);
+            }
+        }
+
+        previousPosition = transform.position;
+    }
+}
